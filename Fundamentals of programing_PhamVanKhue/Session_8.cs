d4910e1b915ea239df37294fb3bcb8587d3e8058
31231026553@ueh.edu.vn <31231026553@ueh.edu.vn>
2024-11-26 20:44:34 +0700

    update

--- Fundamentals of programing_PhamVanKhue/Session_8.cs
+++ Fundamentals of programing_PhamVanKhue/Session_8.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,6 +44,94 @@ namespace Fundamentals_of_programing_PhamVanKhue
                 Console.WriteLine();
             }
         }
-        //Baitap2.Tao mang rang cua vơi cac phan tu ngau nhien
+        //Baitap2.Tao mang rang cua voi cac phan tu ngau nhien
+        public static void Main()
+        {
+            //1. Cho nguoi dung tu nhap hang va cot
+            int column;
+            Console.Write("Nhap so hang cua mang: ");
+            int row = int.Parse(Console.ReadLine());
+            int[][] matran = new int[row][];
+            //2. Nhap du lieu cho tung hang
+            for (int i = 0; i < row; i++)
+            {
+                Console.Write($"Nhap so phan tu cua hang {i}:  ");
+                column = int.Parse(Console.ReadLine());
+                matran[i] = new int[column];//khoi tao mang con voi so cot cu the
+                Random random = new Random();
+                for (int j = 0;j<column;j++)
+                { 
+                    matran[i][j] = random.Next(0,100);
+                }
+            }
+            Inmang(matran);
+            //3.Tim so lon nhat cua tung hang va toan bo ma tran
+            Findmax(matran);
+            //4.sap xep hang theo thu tu tang dan
+            Sortrow(matran);
+            //5.In ra cac phan tu trong mang la so nguyen to 
+            Printprime(matran);
+            Console.WriteLine();
+            //6.Tim va in ra vi tri cua mot so co trong mang
+            Timvitri(matran);
+        }
+        
+        static void Findmax(int[][] a)
+        {
+            int globalmax = int.MinValue;
+            for (int i = 0;i < a.Length;i++)
+            {
+                int rowmax = a[i].Max();
+                Console.WriteLine($"So lon nhat o hang {i} la: {rowmax}");
+                globalmax = Math.Max(rowmax,globalmax);
+            }
+            Console.WriteLine("So lon nhat trong ma tran la " +  globalmax);
+        }
+        static void Sortrow(int[][] a)
+        {
+            for (int i = 0;i<a.Length;i++)
+            {
+                Array.Sort(a[i]);
+            }
+            Console.WriteLine("Mang sau khi sap xep theo thu tu tang dan: ");
+            Inmang(a);
+        }
+        static void Printprime(int[][] a)
+        {
+            Console.WriteLine("Nhung so nguyen to co trong ma tran la: ");
+            foreach (var row in a)
+            {
+                foreach (var num in row)
+                {
+                    if (Isprime(num)) {Console.Write(num + " "); }
+                }
+            }
+        }
+        static bool Isprime(int num)
+        { 
+            if (num <2) return false;
+            if (num == 2 ) return true;
+            for(int i = 2;i*i<=num;i++)
+                {
+                    if(num%i==0) return false;
+                }
+            return true;      
+        }
+        static void Timvitri(int[][] a)
+        {
+            Console.Write("Nhap so can tim trong mang: ");
+            int socantim = int.Parse(Console.ReadLine());
+            for (int i = 0; i<a.Length;i++)
+            {
+                for (int j = 0; j < a[i].Length; j++)
+                { 
+                if(socantim==a[i][j])
+                    {
+                        Console.Write($"{socantim} xuat hien o hang {i} cot {j}");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
     }
 }
---
+++ Fundamentals of programing_PhamVanKhue/Session_9.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals_of_programing_PhamVanKhue
+{
+    internal class Session_9
+    {
+        public static void Main9()
+        {
+            int count = 0;
+            //1.To input a string and print it
+            Console.Write("Nhap vao ban phim mot cau: ");
+            string s1 = Console.ReadLine();
+            Console.WriteLine(s1);
+            //2.Find the length of a string
+            for (int i = 0; i < s1.Length; i++)
+            {
+                count += 1;
+            }
+            Console.WriteLine($"Do dai cua chuoi ky tu la: {count}");
+            //3.Separate individual characters from a string
+            foreach (char c in s1)
+            {
+                Console.WriteLine(c);
+            }
+            //4.to print individual characters of the string in reverse order
+            for (int i = s1.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(s1[i]);
+            }
+            //5.to count the total number of words in a string 
+            int dem = 0;
+            //bo khoang trang thua co o dau va cuoi chuoi
+            s1 = s1.Trim();
+            while (s1.IndexOf(" ") != -1)
+            {
+
+            }
+        }
+        static int CountWords(string s1)
+        {
+                int count = 0;
+                //bo khoang trang thua co trong chuoi
+                s1 = s1.Trim();//bo khoang trang dau va cuoi chuoi
+                while(s1.IndexOf(" ") != -1)
+                {
+                    s1 = s1.Replace("  ", " ");
+                }
+                return count;
+        }
+    }
+}
